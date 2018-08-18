8970f71076a320f67d410ca5abe2d3d8342d66e2
Kevin Dahlhausen <kevin.dahlhausen@gmaiol.com>
2018-08-17 23:40:59 -0400

    added avionics

--- PedalPlaneAvionics/PedalPlaneAvionics.ino
+++ PedalPlaneAvionics/PedalPlaneAvionics.ino
@@ -1,27 +1,17 @@
 // Copyright 2018 by Kevin Dahlhausen
 
-#include <Events.h>
 #include <EventQueue.h>
 #include <EventDispatcher.h>
 
-#include "switch.h"
+#include "avionics_events.h"
 #include "interpreter.h"
-#include "sound.h"
 #include "machineguns.h"
 #include "motor.h"
+#include "sound.h"
+#include "switch.h"
 
 EventQueue q;
 EventDispatcher event_dispatcher(&q);
-
-
-// events
-enum AvionicsEvents {
-
-    MOTOR_START,
-    MOTOR_STOP,
-    MACHINEGUNS_START,
-    MACHINEGUNS_STOP,
-};
 
 // input objects
 Switch motor_switch(0, 0, MOTOR_START, -1);
---
+++ PedalPlaneAvionics/avionics_events.h
@@ -0,0 +1,8 @@
+// Copyright 2018 by Kevin Dahlhausen
+enum AvionicsEvents
+{
+    MOTOR_START,
+    MOTOR_STOP,
+    MACHINEGUNS_START,
+    MACHINEGUNS_STOP,
+};
