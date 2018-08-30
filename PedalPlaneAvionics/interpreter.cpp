32b7d4957274ba013d3e8070fafd7d04abfc1cc1
kdahlhaus <kevin.dahlhausen@gmaiol.com>
2018-08-29 22:32:02 -0400

    unrec command to interp

--- PedalPlaneAvionics/interpreter.cpp
+++ PedalPlaneAvionics/interpreter.cpp
@@ -7,9 +7,11 @@
 
 void onboard_LED_on() { Log.trace(F("sending on\n")); send_event(ONBOARD_LED_ON); }
 void onboard_LED_off() { Log.trace(F("sending off\n")); send_event(ONBOARD_LED_OFF); }
+void unrecognized(const char *command) { Log.trace("unrecognized command: %s", command); }
 
 SerialInterpreter::SerialInterpreter()
 {
     serial_command.addCommand("ol1", onboard_LED_on);
     serial_command.addCommand("ol0", onboard_LED_off);
+    serial_command.setDefaultHandler(unrecognized);
 }
