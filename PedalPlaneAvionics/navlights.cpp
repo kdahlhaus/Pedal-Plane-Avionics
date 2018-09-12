68f652eb6709911579ba226b7d414cdae2a7856a
kdahlhaus <kevin.dahlhausen@gmaiol.com>
2018-09-11 23:25:05 -0400

    adjusted flash rate

--- PedalPlaneAvionics/navlights.cpp
+++ PedalPlaneAvionics/navlights.cpp
@@ -6,7 +6,7 @@
 #include <ArduinoLog.h>
 #include <Curve.h>
 
-#define FADE_TIME_MS 1000
+#define FADE_TIME_MS 1160
 
 Navlights::Navlights() :
     lights_on(false), fader(6)
@@ -17,39 +17,27 @@ Navlights::Navlights() :
     register_event_listener(NAVLIGHTS_OFF, makeFunctor((EventListener *)0, (*this), &Navlights::onEvent));
 }
 
-void Navlights::on() { lights_on = true; fader.set_value(0); fader.fade(255, 3000); Log.trace(F("navlights on")); } 
+void Navlights::on() { lights_on = true; fader.set_value(0); fader.fade(255, FADE_TIME_MS); Log.trace(F("navlights on")); } 
 void Navlights::off() { lights_on = false; fader.set_value(0); Log.trace(F("navlights off")); } 
 
 
-float rate = 1100;
 
 void Navlights::update()
 {
     if (lights_on)
     {
-        // temp to adjust rate remove when determined
-        float newrate = analogRead(15);
-        newrate = newrate / 1024;
-        newrate = newrate * 4000;
-        if (abs(newrate - rate) > 5)
-        {
-            Log.trace(F("nl fader old rate:%d, new:%d\n"), (int)(rate), (int)(newrate));
-            rate = newrate;
-        }
-
-        
         fader.update();
         if (!fader.is_fading())
         {
             // Fade from 255 - 0
             if (fader.get_value() == 255)
             {
-              fader.fade(0, (int)(rate));
+              fader.fade(0, FADE_TIME_MS);
             }
             // Fade from 0 - 255
             else
             {
-              fader.fade(255, (int)(rate));
+              fader.fade(255, FADE_TIME_MS);
             }
         }
     }
